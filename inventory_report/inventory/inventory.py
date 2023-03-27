5df45ec81eb7f5825563f24a15dee81a7ed411ac
JaySant <jaysant15@gmail.com>
2023-03-27 18:32:10 -0300

    requisito 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,9 +7,15 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @staticmethod
     def import_data(path: str, report_type: str):
-        with open(path) as csv_file:
-            reader = csv.DictReader(csv_file)
-            data = list(reader)
+        if path.endswith(".csv"):
+            with open(path) as csv_file:
+                reader = csv.DictReader(csv_file)
+                data = list(reader)
+        elif path.endswith(".json"):
+            with open(path) as json_file:
+                data = json.load(json_file)
+        else:
+            raise ValueError("Arquivo invalido")
 
         if report_type == 'simples':
             return SimpleReport.generate(data)
