5b425065af89051c3cd4454c286fd8b19f794423
JaySant <jaysant15@gmail.com>
2023-03-27 15:24:49 -0300

    requisito 3 e test: requisito 8

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,24 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data_list):
+        report = SimpleReport.generate(data_list)
+
+        companies = {}
+        for item in data_list:
+            company = item["nome_da_empresa"]
+            if company in companies:
+                companies[company] += 1
+            else:
+                companies[company] = 1
+
+        product_counts = "\n".join([
+            f"- {company}: {count}\n" for company, count in companies.items()])
+
+        return (
+            f"{report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{product_counts}"
+        )
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto="farinha",
+        nome_da_empresa="Farinini",
+        data_de_fabricacao="01-05-2021",
+        data_de_validade="02-06-2023",
+        numero_de_serie="ASBCD",
+        instrucoes_de_armazenamento="ao abrigo de luz"
+
+    )
+
+    assert str(product) == (
+        "O produto farinha fabricado em 01-05-2021 por Farinini com "
+        "validade até 02-06-2023 precisa ser armazenado ao abrigo de luz."
+    )
