882bc2f01745b0a9b4fd90b4e0af0eedf680ee05
JaySant <jaysant15@gmail.com>
2023-03-28 02:45:33 -0300

    testando requisito 11

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,13 @@
+from inventory_report.inventory.inventory import Inventory
+import sys
+
+
 def main():
-    pass
+
+    try:
+        path = sys.argv[1]
+        report_type = sys.argv[2]
+        data = Inventory().import_data(path, report_type)
+        print(data, end="")
+    except IndexError:
+        print("Verifique os argumentos", file=sys.stderr)
